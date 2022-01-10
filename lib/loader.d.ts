d3aea87d01ff892df2b381d1f0b3614dcab4b7eb
Simon <simon.cheng@advantech.com.tw>
2022-01-10 10:34:49 +0800

    Compile ts files

--- lib/loader.d.ts
+++ lib/loader.d.ts
@@ -1,3 +1,3 @@
 import { wasm_request_type } from './interface';
 import loader, { ASUtil } from '@assemblyscript/loader';
-export declare function wasm_init(wasm_request: wasm_request_type | wasm_request_type[], memory?: WebAssembly.Memory, custom_imports?: loader.Imports): Promise<ASUtil>;
+export declare function wasm_init(wasm_request: wasm_request_type | wasm_request_type[], memory?: WebAssembly.Memory, custom_imports?: loader.Imports, base_url?: string): Promise<ASUtil>;
